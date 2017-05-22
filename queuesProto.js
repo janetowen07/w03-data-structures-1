cf87353d2287869c4b96818293f516c3515b4b7d
Janet <janetowen07@gmail.com>
2017-05-22 17:53:04 +0100

    works for queue prototype

---
+++ queuesProto.js
@@ -0,0 +1,20 @@
+function queuesProto () {
+    const obj = Object.create(queuesProto.prototype);
+        obj.queue = {};
+        obj.frontIndex = 0;
+        obj.rearIndex = 0;
+        return obj;
+}
+
+queuesProto.prototype.push = function (item) {
+            this.queue[this.rearIndex] = item;
+            this.rearIndex++;
+        };
+queuesProto.prototype.shift = function () {
+            var temp = this.queue[this.frontIndex];
+            delete this.queue[this.frontIndex];
+            this.frontIndex++;
+            return temp;
+        };
+
+module.exports = queuesProto;
